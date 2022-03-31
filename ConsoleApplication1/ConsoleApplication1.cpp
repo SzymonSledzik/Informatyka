453fbf75e17df93298dacb89fa087133c7300f38
szymonsledzik <szymon.sledzik02@gmail.com>
2022-03-31 11:37:58 +0200

    Dodanie NWW NWD

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int NWD(int a, int b){
+    while (a != b) {
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+    }
+    return a;
+}
+
+int main()
+{
+    int a, b;
+
+    cout << "Podaj Libczę a: ";
+    cin >> a;
+    cout << endl << "Podaj liczbę b: ";
+    cin >> b;
+    cout << "NWD: "<< NWD(a, b);
+    cout << ", NWW: " << (a*b)/NWD(a, b);
+
+}
---
+++ PrimeNumbs/Prime/Prime.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+void isPrime(int x){
+    int k = 0;
+    int p = 2;
+    bool running = true;
+    while (k <= x) {
+        running = true;
+        for (int j = 2; j <= p - 1; j++) {
+            if (p % j == 0) {
+                running = false;
+            }
+        }
+
+        if (running == true) {
+
+            cout << p << ", ";
+            k++;
+        }
+        p++;
+    }
+}
+
+int main()
+{
+    int x; 
+    cout << "Podaj liczbę do sprawdzenia: ";
+    cin >> x;
+    cout << endl;
+    isPrime(x);
+}
