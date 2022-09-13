c1c615f4f48a02290e8d38b7daa09b0faef036dc
szymonsledzik <szymon.sledzik02@gmail.com>
2022-09-13 12:47:52 +0200

    Hello World PAI

---
+++ Pai/Hello World/index.php
@@ -0,0 +1,26 @@
+<style>
+p{
+    color: red;
+}
+</style>
+<html lang="pl">
+        <meta charset="UTF-8">
+        <title>PHP</title>
+    </head> 
+    <header>
+
+    </header>
+
+    <body>    
+        <?php
+        $x = 1;
+        echo '<p> Hello World<br>My counter ',$x,'<br>Hello friend</p>'
+                                    ;
+        ?>
+    </body>
+
+    <footer>
+
+    </footer>
+</html>
+
